6e8e7f8d1c96f5149b169521b98b5e36eddcdc46
dfemanuel <df.emanuel@hotmail.com>
2020-09-13 21:04:07 -0300

    Adição do Circuit Breaker

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { setupCircuitBreaker } = require("./setupCircuitBreaker");
+
 var express = require('express'),
   	app = express(),
 	port = process.env.PORT || 3000;
@@ -6,11 +8,21 @@ var express = require('express'),
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
 
-
 	var routes = require('./api/routes/calcRoutes'); //importing route
 	routes(app); //register the route
 
 	app.listen(port);
+
+	// circuit breaker
+	const circuit = setupCircuitBreaker();
+
+	app.use('/', (request, response) => {
+	    circuit.fire().then(result => {
+	        response.send(result);
+	    }).catch(err => {
+	        response.send(err.message);
+	    });
+	});
 
 	app.use(function(req, res) {
 	  res.status(404).send({url: req.originalUrl + ' not found'})
